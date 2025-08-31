7b799df6126bdcec27fce543992e119b0080f688
Shams <shamsmedhat1@gmail.com>
2025-09-01 02:26:25 +0300

    chore(auth): Add the header name in vercel of token.

--- src/lib/utils/get-token.ts
+++ src/lib/utils/get-token.ts
@@ -2,7 +2,9 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export const getTokenDecoded = async () => {
-  const headerToken = (await cookies()).get("next-auth.session-token")?.value;
+  const headerToken =
+    (await cookies()).get("__Secure-next-auth.session-token")?.value ??
+    (await cookies()).get("next-auth.session-token")?.value;
 
   const token = await decode({
     token: headerToken,
